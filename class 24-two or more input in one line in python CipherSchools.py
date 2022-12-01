37dfb816d4449bad7cf3dbec1ff87e50e984d8cb
funsook <119058491+funsook@users.noreply.github.com>
2022-12-01 05:45:19 -0800

    Create class 24-two or more input in one line in python CipherSchools.py

---
+++ class 24-two or more input in one line in python CipherSchools.py
@@ -0,0 +1,7 @@
+name, rollno=input("Enter your name and rollno ").split()
+print(name)  
+print(rollno) 
+
+name, rollno=input("Enter your name and rollno ").split(",")
+print(name)  
+print(rollno)
