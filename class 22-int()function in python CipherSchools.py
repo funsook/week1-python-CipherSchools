98c641df4c09ca47b49122869a5476e835c73e8f
funsook <119058491+funsook@users.noreply.github.com>
2022-12-01 05:48:08 -0800

    Create class 22-int()function in python CipherSchools.py

---
+++ class 22-int()function in python CipherSchools.py
@@ -0,0 +1,14 @@
+num_1 = input("Enter 1st no.") #8
+num_2 = input("Enter 2nd no.")#8
+total = num_1 + num_2
+print("Total no. is " + total) #88
+
+num_1 = int(input("Enter 1st no.")) #8
+num_2 = int(input("Enter 2nd no."))#8
+total = num_1 + num_2
+print("Total no. is " + str(total)) #16
+
+num1 = str(4)
+num2 = int("44")
+num3 = float("33")
+print(num2 + num3) #77
