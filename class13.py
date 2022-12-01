520a941873d18ebcbcf1f7bc1abca1fd2dbb5d16
funsook <119058491+funsook@users.noreply.github.com>
2022-12-01 05:18:23 -0800

    Create class13.py

---
+++ class13.py
@@ -0,0 +1,12 @@
+#*print these following lines **
+'''this is \\ double slash
+these are /\/\/\/\ mountains
+he is   awesome (use escape )
+\" \n \t \'(print this as an output)
+'''
+
+print("\\")
+print("ritika is\tawesome girl")
+print("ritika\"")
+print("ritika\t")
+print("ritika\nKumari")
