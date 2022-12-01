9b0425de79966a92b94b37b524553c10ce6de321
funsook <119058491+funsook@users.noreply.github.com>
2022-12-01 05:43:55 -0800

    Create class 25-string formatting CipherSchools.py

---
+++ class 25-string formatting CipherSchools.py
@@ -0,0 +1,11 @@
+name = "Ritika"
+age = 18
+print("My name is " + name + " and my age is " + str(age))
+print("My name is {} and my age is {} ".format(name,age)) #string_formatting
+
+Class = "Tenth"
+section = 9
+print("Your class is {} ,and section is {} ".format(Class,section)) 
+
+print(f"hello {name} my age is {age}") 
+print(f"hello {name} my age is {age+45}")
