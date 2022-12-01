97687e97b501bf0ba2724d7815b29232e2693270
funsook <119058491+funsook@users.noreply.github.com>
2022-12-01 05:22:22 -0800

    Create class11.py

---
+++ class11.py
@@ -0,0 +1,14 @@
+comments
+print("hello \"world\" world")
+print('I\'m Ritika')
+
+print("line A")
+print("line B")
+#print("line A\nline B\n line C")
+
+#print("name\Ritika")
+
+#print("this is backslash\\")
+print("this is double backslash\\\\")
+
+print("hell\bro
